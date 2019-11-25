4ff2e7eeb3c086a0a7faad750bb1c63c13c0e036
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2019-11-25 17:59:03 +0900

    add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This application was built by<br/>Matsui Hidetoshi.</h1>
+    <h2>Versions</h2>
+    <h3>Frontend</h3>
+    <p>node 12.10.0</p>
+    <p>npm 6.11.3</p>
+    <p>vue-cli 3.10.0</p>
+    <h3>Backend</h3>
+    <p>AWS Lambda</p>
+    <p>python 3.7</p>
+    <h3>Versioning</h3>
+    <p>Github</p>
   </div>
 </template>
+<style scoped>
+div {
+  margin-bottom: 150px;
+}
+</style>
